8f766eaa73d20093c85fff1ced56b65b0d524f06
Billiamd1012 <william.darker2003@gmail.com>
2025-04-20 16:35:03 +0930

    Added map implementation

--- DeadMansDraw/DeadMansDraw/Cards/Map.cpp
+++ DeadMansDraw/DeadMansDraw/Cards/Map.cpp
@@ -1,4 +1,7 @@
 #include "Map.h"
+#include "../Game.h"
+#include <iostream>
+
 
 Map::Map(int _value) : value(_value) {
 	suit = Suit::Map;
@@ -9,7 +12,29 @@ int Map::getValue() {
 }
 
 void Map::play(const Game& _game, Player& player) {
+	Discard* discardPile = _game.currentPlayer->getDiscardPile();
+	//if size of 
+	if (discardPile->discardedCards.size() > 0) {
+		//draw top 3 cards from the discard pile
+		int maxToAdd = std::min((int)discardPile->discardedCards.size(), 3);
 
+		for (int i = 0; i < maxToAdd; ++i) {
+			std::cout<< "(" << i << ")" << (discardPile->discardedCards[i].get()->toString(false));
+		}
+
+		// Ask the player to choose one to discard
+		int choice = -1;
+		do {
+			std::cout << "\tWhich card do you pick? ";
+			std::cin >> choice;
+		} while (choice < 0 || static_cast<size_t>(choice - 1) >= discardPile->discardedCards.size());
+
+		// Remove and move the card
+		std::unique_ptr<Card> removedCard = std::move(discardPile->discardedCards[choice-1]);
+		std::cout << "\tYou played the " << removedCard.get()->toString(false) << "\n";
+		discardPile->discardedCards.erase(discardPile->discardedCards.begin() + choice-1);
+		_game.currentPlayer->playCard(std::move(removedCard));
+	}
 }
 
 Suit Map::type() {
